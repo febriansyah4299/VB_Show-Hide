cd8b649371168d851d41b19390d6b29e67f97c26
FEBRIANSYAH.ID <febriansyah4299@yahoo.com>
2018-03-19 11:50:57 +0700

    .

---
+++ FEBRIANSYAHShowHide/MainWindow.xaml.vb
@@ -0,0 +1,22 @@
+﻿Class MainWindow
+    Private Sub button_Red_Click(sender As Object, e As RoutedEventArgs) Handles button_Red.Click
+        button_Green.Visibility = True
+        button_Blue.Visibility = True
+    End Sub
+
+    Private Sub button_Green_Click(sender As Object, e As RoutedEventArgs) Handles button_Green.Click
+        button_Red.Visibility = True
+        button_Blue.Visibility = True
+    End Sub
+
+    Private Sub button_Blue_Click(sender As Object, e As RoutedEventArgs) Handles button_Blue.Click
+        button_Red.Visibility = True
+        button_Green.Visibility = True
+    End Sub
+
+    Private Sub button_Back_Click(sender As Object, e As RoutedEventArgs) Handles button_Back.Click
+        button_Red.Visibility = False
+        button_Green.Visibility = False
+        button_Blue.Visibility = False
+    End Sub
+End Class
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# VB_Show-Hide
-VB Show &amp; Hide by FEBRIANSYAH.ID
+# VB Show & Hide
+VB Show & Hide by <a href="https://febriansyah.id/?utm_source=github&utm_medium=github_readme_text&utm_campaign=refer">FEBRIANSYAH.ID</a>
+<p align="center"><a href="https://febriansyah.id/?utm_source=github&utm_medium=github_readme_banner&utm_campaign=refer"><img src="https://febriansyah.id/wp-content/uploads/2017/09/febriansyahid-logo-web.png"></a></p>
+
+<p align="center">
+<a href="https://travis-ci.org/febriansyah4299/VB_Show-Hide"><img src="https://travis-ci.org/febriansyah4299/VB_Show-Hide.svg" alt="Status"></a>
+</p>
